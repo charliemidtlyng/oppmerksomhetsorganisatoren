670c65c11cdd18078a1931cca2f2f677c03bccb4
Charlie Midtlyng <Charlie.Midtlyng@BEKK.no>
2013-05-18 00:27:14 +0200

    Updated conf in order to start on heroku

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -16,9 +16,8 @@ CREATE TABLE person (
     navn varchar(64),
     fodselsdato varchar(255),
     adresseId bigint(4),
-    info varchar(1023),
-    FOREIGN KEY (adresseId ) REFERENCES adresse(id)
-
+    info varchar(1023)
+    FOREIGN KEY (adresseId) REFERENCES adresse(id)
 );
 
 # --- !Downs
