1936320240623537e4da72a7a3a71b4167ae5c4c
Charlie Midtlyng <Charlie.Midtlyng@BEKK.no>
2013-05-20 15:01:11 +0200

    Bah

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -8,13 +8,19 @@ CREATE TABLE adresse (
     postnummer varchar(4),
     poststed varchar(255)
 );
+
 CREATE SEQUENCE person_id_seq;
 CREATE TABLE person (
     id bigint NOT NULL DEFAULT nextval('person_id_seq') PRIMARY KEY,
     navn varchar(64),
-    fodselsdato varchar(255),
+    fodselsdato timestamp,
     adresseId bigint REFERENCES adresse(id),
     info varchar(255)
 );
 
 # --- !Downs
+DROP TABLE person;
+DROP SEQUENCE person_id_seq;
+
+DROP TABLE adresse;
+DROP SEQUENCE adresse_id_seq;
